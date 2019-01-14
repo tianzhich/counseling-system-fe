65afa3fabcef20b432a7ad51c8c8926efcc3dab7
tianzhi <tianzhi@kuaishou.com>
2019-01-14 18:09:08 +0800

    update: 完善ast

--- scripts/ts-ast.js
+++ scripts/ts-ast.js
@@ -1,7 +1,6 @@
 const { Project } = require('ts-simple-ast')
 const _ = require('lodash/string')
 const path = require('path')
-const ts = require('typescript')
 
 const project = new Project();
 const srcDir = path.join(__dirname, '..', 'src', 'common');
@@ -15,9 +14,16 @@ async function addRoute(featureName) {
       moduleSpecifier: `../features/${featureName}/route`
     })
     const routeVar = srcFile.getVariableStatementOrThrow('childRoutes');
-    // console.log(routeVar.getChildCount());
-    const routeArr = routeVar.getLastChildByKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression);
-    routeArr.addElement(`${FeatureName}Route`);
+
+    let newRoutes = [];
+
+    routeVar.getDeclarations().forEach((decl, i) => {
+      console.log(i);
+      decl.getInitializer().forEachChild(node => {
+        newRoutes.push(node.getText());
+      })
+      decl.setInitializer(`[${newRoutes.join(', ')}, ${FeatureName}Route]`);
+    })
 
     await project.save();
     console.log("Add route successful");
