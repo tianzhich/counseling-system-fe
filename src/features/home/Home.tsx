4e0b03747c7d59dc73bb7a0874865ca564188d13
tianzhi <tianzhi@kuaishou.com>
2019-01-24 20:19:49 +0800

    add: fake api call using axios

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 interface IHomeProps {}
 
@@ -8,6 +9,12 @@ export default class Home extends React.Component<IHomeProps, IHomeState> {
     constructor(props: IHomeProps) {
         super(props);
         this.state = {};
+    }
+
+    public componentDidMount() {
+        // axios.get('http://172.17.29.96:8080/api').then(res => {
+        //     console.log(res);
+        // })
     }
 
     render() {
