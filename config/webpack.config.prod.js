bd4e0764f2fe0efa6984254187ee92b453863f25
tianzhich <tianzhi@kuaishou.com>
2019-04-19 21:18:24 +0800

    feat: cookie返回存在的一些问题

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -324,7 +324,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', generateStatsFile: true }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ set nginx_dest_path /usr/local/webserver/nginx/conf
 # 防止build时间过久自动退出，设置无限时长替代默认的30s
 set timeout -1
 
-spawn npm run build
-expect eof
+# spawn npm run build
+# expect eof
 
 # solve https://stackoverflow.com/questions/25791699/automate-scp-with-multiple-files-with-expect-script
 spawn bash -c "scp -r $build_src_path $user@$host:$build_dest_path"
